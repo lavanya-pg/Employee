f82741b5e0689b33c7b4d7b91a7c1103fe32ee62
lavanya-pg <lavanyapg8@gmail.com>
2021-06-26 15:58:46 +0530

    ADDED:Partime

--- src/com/Employee/Employee.java
+++ src/com/Employee/Employee.java
@@ -9,6 +9,8 @@ public class Employee
 		int Fullday=8;
 		int Wageperhour=20;
 		int dailywage;
+		int parttime=4;
+		
 		System.out.println("Welcome to the Employee Wage Computation Program");
 		Random ran=new Random();
 		int ran1 = ran.nextInt(2);
@@ -17,6 +19,12 @@ public class Employee
 			System.out.println("Employee is Present");
 			dailywage=Fullday*Wageperhour;
 			System.out.println("Employee Wage is" +dailywage);
+		}
+		else if (ran1 == 2)
+		{
+			System.out.println("Employee is parttime present");
+			dailywage=parttime*Wageperhour;
+			System.out.println("Employee wage is" +dailywage);
 		}
 		else
 			System.out.println("Employee is Absent");
