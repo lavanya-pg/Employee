7470ffcb68a2c33f328625c5a8825e90d73c3923
lavanya-pg <lavanyapg8@gmail.com>
2021-06-26 15:47:34 +0530

    ADDED:Checking Employee Is Present

--- src/com/Employee/Employee.java
+++ src/com/Employee/Employee.java
@@ -1,9 +1,19 @@
 package com.Employee;
 
-public class Employee {
+import java.util.Random;
 
-	public static void main(String[] args) {
+public class Employee
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Welcome to the Employee Wage Computation Program");
+		Random ran=new Random();
+		int ran1 = ran.nextInt(2);
+		if (ran1 == 1)
+			System.out.println("Employee is Present");
+		else
+
+				System.out.println("Employee is Absent");
 	}
 
 }
