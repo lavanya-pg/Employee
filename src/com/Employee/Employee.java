2f8675254c7c3c656e2cfc9e2dcefa25c940a7b1
lavanya-pg <lavanyapg8@gmail.com>
2021-06-26 15:53:49 +0530

    ADDED:dailywage and workinghours

--- src/com/Employee/Employee.java
+++ src/com/Employee/Employee.java
@@ -5,15 +5,21 @@ import java.util.Random;
 public class Employee
 {
 	public static void main(String[] args)
-	{
+	{	
+		int Fullday=8;
+		int Wageperhour=20;
+		int dailywage;
 		System.out.println("Welcome to the Employee Wage Computation Program");
 		Random ran=new Random();
 		int ran1 = ran.nextInt(2);
 		if (ran1 == 1)
+		{
 			System.out.println("Employee is Present");
+			dailywage=Fullday*Wageperhour;
+			System.out.println("Employee Wage is" +dailywage);
+		}
 		else
-
-				System.out.println("Employee is Absent");
-	}
+			System.out.println("Employee is Absent");
+	 }
 
 }
